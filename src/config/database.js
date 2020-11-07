ca6bc8825a5a6b065fe6d5390410a3f4088d7c29
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-11-07 18:44:01 -0300

    Adds sequelize and migrations

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  define: {
+    timestamps: false,
+    underscored: false,
+  },
+}
