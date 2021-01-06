6976cbb19a8c3b9c4ae0853822168482a783f129
gabriel-pagotto <gabriel.pagotto@icloud.com>
2021-01-06 13:48:10 -0300

    Configure database

--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,18 @@ dotenv.config();
 module.exports = {
   dialect: process.env.DATABASE_DIALECT,
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   define: {
     timestamps: false,
-    underscored: false,
+    underscored: true,
+  },
+  dialectOptions: {
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
   },
 }
