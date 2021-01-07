022970f94934f5b77556f76ff26765a17b9c568b
gabriel-pagotto <gabriel.pagotto@icloud.com>
2021-01-06 22:06:40 -0300

    Change https redirect

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ require('./src/database');
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.use((request, response, next) => {
-  const inProduction = process.env.ON_PRODUCTION === 'true';
-  const isHttps = (request.headers["x-forwarded-proto"] || "").endsWith("http");
-
-  isHttps && inProduction ? response.redirect(`https://${request.hostname}${request.url}`) : next();
-});
 
 app.use(router);
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use((request, response, next) => {
+  const inProduction = process.env.ON_PRODUCTION === 'true';
+  const isHttps = (req.headers["x-forwarded-proto"] || "").endsWith("http");
+  
+  isHttps && inProduction ? response.redirect(`https://${request.hostname}${request.url}`) : next();
+});
 
 require('./src/routes')(app);
 
